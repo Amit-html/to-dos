128567d0e999bf81ecebae276c4e6c3295db0c10
Amit Kumar <120585013+Amit-html@users.noreply.github.com>
2025-09-13 21:43:11 +0530

    updating the function

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+  function NewInput(e){
+    setTask(e.target.value);
+    console.log(task);
+  }
+  function addTask(){
+    setTaskList([...taskList, task]);
+    console.log(taskList);
+    setTask('');
+  }
+  
   return (
     <>
-     <div>
-      <h1>hello world</h1>
-     </div>
+    <ul>
+    {taskList.map((t, index) => (
+    <li key={index}>{t}</li>
+  ))}
+    </ul>
+    <p>{task}</p>
+    <input type="text" name="input" id="newInput"  onChange={NewInput}/>
+    <button onClick={addTask}>Add</button>
     </>
   )
 }
